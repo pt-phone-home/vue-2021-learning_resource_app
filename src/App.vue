e8951d8da29ce4f1e7255af30a5cee3f119b307e
= <=>
2021-03-04 15:00:43 +0000

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import StoredResources from './components/learning-resources/StoredResources'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    StoredResources,
+  },
+  data () {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js Documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google... ',
+          link: 'https://google.ie',
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,19 @@
+<template>
+     <ul>
+    <learning-resource v-for="item in resources" :key="item.id"
+    :id="item.id"
+    :title="item.title"
+    :description="item.description"
+    :link="item.link"
+    ></learning-resource>
+  </ul>
+</template>
+<script>
+import LearningResource from './LearningResource';
+export default {
+    props: ['resources'],
+    components: {
+        LearningResource,
+    }
+}
+</script>
